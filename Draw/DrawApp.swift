e3cecbf3c53e39be4132b090cfa07765a0ec75ab
Rishi Jansari <jansaririshi@gmail.com>
2024-07-25 13:40:55 +0100

    Initial Commit

---
+++ Draw/DrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawApp.swift
+//  Draw
+//
+//  Created by Rishi Jansari on 25/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct DrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
